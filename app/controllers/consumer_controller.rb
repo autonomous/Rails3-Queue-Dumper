d9e1f9ed9ecabedf1c0b78676d28247033afa15c
Christopher Spring <chris.spring@gmail.com>
2010-07-08 07:16:06 +0200

    ensure success for telemetry comms

--- app/controllers/consumer_controller.rb
+++ app/controllers/consumer_controller.rb
@@ -2,9 +2,10 @@ class ConsumerController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def munch
     Telemetry.create!(:data => params[:json])
-    head(:ok)
   rescue => e
     logger.error e.backtrace
+  ensure
+    head(:ok)
   end
 
 end
